e3d568cb1b26e79c05a1e77e392db3458b58554b
CesarMR5 <cesarmirr315@gmail.com>
2024-10-16 20:10:49 -0600

    Terminando el index

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -4,15 +4,39 @@ export const links = [
     text: "Inicio",
   },
   {
-    url: "./filters",
-    text: "Filtros",
-  },
-  {
     url: "./transform",
     text: "Transformar",
   },
   {
     url: "./background",
     text: "Cambiar Fondos",
+  },
+  {
+    url: "./total",
+    text: "Cambio Completo",
+  },
+];
+
+export const cardInfo = [
+  {
+    title: "Transformaciones Monstruosas",
+    description:
+      "Transformate a ti, tus amigos, o todas las personas de tus fotos en criaturas escalofriantes.",
+    link: "./transform",
+    image: "transform",
+  },
+  {
+    title: "Fondos Espeluznantes",
+    description:
+      "Transforma todo a tu alrededor en fondos llenos de monstruos, zombies, lugares horripilantes, y muchas cosas mas.",
+    link: "./background",
+    image: "background",
+  },
+  {
+    title: "Cambios Totales",
+    description:
+      "Combina lo mejor de ambos mundos! Transforma a las personas de tus fotos y añade fondos aterradores a tus imágenes con un solo click.",
+    link: "./total",
+    image: "total",
   },
 ];
